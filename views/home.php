a05bc4458ca633ed06c608004eec823e62a3600b
Magno <workmag@outlook.com>
2018-03-19 22:54:16 -0300

    Desenvolvimento menu superior e rodapé

---
+++ views/home.php
@@ -0,0 +1,135 @@
+<!--
+  Autor: Magno
+  Data de modificação: 19/03/2018
+  Obs: Página principal contém menu e rodapé para inserir as outras páginas
+  -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Home - Bem vindo</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/normalize.css">
+  </head>
+  <body>
+    <!-- Menu Superior -->
+    <nav>
+      <div class="menu-container">
+        <!-- Área com Itens e Logo do menu superior -->
+        <div class="conteudo-menu">
+          <div class="itens-menu">
+            Empresa
+            <div class="">
+
+            </div>
+          </div>
+          <div class="itens-menu">
+            <h2>itens</h2>
+          </div>
+          <div class="logo-menu">
+            <a href="index.php"><h1><img src="img/logo.png" alt="Logo"></h1></a>
+          </div>
+          <div class="itens-menu">
+            <h2>itens</h2>
+          </div>
+          <div class="itens-menu">
+            <h2>itens</h2>
+          </div>
+        </div>
+        <!--  -->
+        <div class="login-menu">
+          Login
+        </div>
+      </div>
+    </nav>
+
+
+    <!-- Conteúdo da página -->
+    <div class="conteudo">
+
+    </div>
+
+
+    <!-- rodapé -->
+    <footer>
+      <div class="footer-container">
+        <!-- Caixa com todo o conteudo do rodapé -->
+
+        <div class="conteudo-footer">
+          <!-- Titulos rodapé -->
+          <div class="links-footer">
+            <table>
+              <tr>
+                <td>
+                  <h3>Empresa</h3>
+                </td>
+                <td>
+                 <h3>Legislação</h3>
+                </td>
+                <td>
+                 <h3> Agência</h3>
+                </td>
+                <td>
+                 <h3> Recursos Humanos</h3>
+                </td>
+                <td>
+                 <h3> Contatos</h3>
+                </td>
+              </tr>
+              <!-- Links de Acesso -->
+              <tr>
+                <td>
+                  <ul>
+                    <li> <a href="#">História</a> </li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li> <a href="#">Informações</a> </li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li> <a href="#">Encontre uma rodovia</a> </li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li> <a href="#">Trabalhe Conosco</a> </li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li> <a href="#">Fale Conosco</a> </li>
+                  </ul>
+                </td>
+              </tr>
+            </table>
+            <div class="campo-redes-sociais">
+              <div class="redes-sociais">
+                <img src="img/icon/facebook.png" alt="">
+              </div>
+              <div class="redes-sociais">
+                <img src="img/icon/instagram.png" alt="">
+              </div>
+              <div class="redes-sociais">
+                <img src="img/icon/youtube.png" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="">
+            <div class="pagamento-footer">
+              <div class="">
+                asdf
+              </div>
+            </div>
+            <div class="contato-footer">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <script src="js/jquery-3.3.1.js"></script>
+  </body>
+</html>
