361ebe339848972f6c3d76ea9a1172869820fa79
Magno <workmag@outlook.com>
2018-04-16 11:29:53 -0300

    Adicionada api (node) e estilização do scrollbar css

---
+++ api/index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+const mysql = require('mysql');
+const db = require('./models/db');
+
+//Configurando o body parser para pegar POSTS mais tarde
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+// Definindo Rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando'}));
+app.use('/', router);
+
+// Lista todos os funcionários da empresa
+router.get('/v1/funcionarios', (req, res) => {
+  db.execSQLQuery('SELECT * FROM funcionario', res);
+});
+
+// Busca um usuário por id no banco
+router.get('/v1/funcionarios/:id?', (req, res) =>{
+    let filter = '';
+    if(req.params.id) filter = ' WHERE id=' + parseInt(req.params.id);
+    db.execSQLQuery('SELECT * FROM funcionario' + filter + ' LIMIT 1', res);
+});
+
+// Exclui Funcionário do banco
+router.delete('/v1/funcionarios/:id?', (req, res) =>{
+    let filter = '';
+    if(req.params.id) filter = ' WHERE id=' + parseInt(req.params.id);
+    db.execSQLQuery('DELETE FROM funcionario' + filter + ' LIMIT 1', res);
+});
+
+//Insere funcinário no banco de dados
+router.post('/v1/funcionarios', (req,res) => {
+  const id = parseInt(req.params.id);
+  const nome = req.body.nome.substring(0,150);
+  const email = req.body.email.substring(0, 45);
+  const cpf = req.body.cpf.substring(0, 11);
+  const senha = req.body.senha.substring(0, 45);
+  const datanasc = req.body.datanasc.substring(0, 45);
+  const sexo = req.body.sexo.substring(0, 45);
+  const rg = req.body.rg.substring(0, 45);
+  const ativo = req.body.ativo.substring(0, 45);
+  const login = req.body.login.substring(0, 45);
+  db.execSQLQuery(`INSERT INTO funcionario(nome, email, cpf, senha, datanasc, sexo, rg, ativo,login) VALUES('${nome}', '${email}','${cpf}', '${senha}', '${datanasc}','${sexo}','${rg}','${ativo}', '${login}')`, res);
+});
+
+// Atualiza um funcionários
+router.patch('/v1/funcionarios/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    const nome = req.body.nome.substring(0,150);
+    const email = req.body.email.substring(0, 45);
+    const cpf = req.body.cpf.substring(0, 11);
+    const senha = req.body.senha.substring(0, 45);
+    const datanasc = req.body.datanasc.substring(0, 45);
+    const sexo = req.body.sexo.substring(0, 45);
+    const rg = req.body.rg.substring(0, 45);
+    const ativo = req.body.ativo.substring(0, 45);
+    const login = req.body.login.substring(0, 45);
+
+    db.execSQLQuery(`UPDATE funcionario SET nome='${nome}', cpf='${cpf}', email='${email}', senha='${senha}', datanasc='${datanasc}', sexo='${sexo}', rg='${rg}', ativo='${ativo}', login='${login}'  WHERE ID=${id}`, res);
+});
+
+
+// inicia o servidor
+app.listen(port);
+console.log('Api Funcionando!');
---
+++ api/models/db.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const mysql = require('mysql');
+
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : 3306,
+    user     : 'root',
+    password : 'bcd127',
+    database : 'db_asteroide'
+  });
+
+
+  module.exports = {
+    execSQLQuery:  function(sqlQry, res){
+        connection.query(sqlQry, function(error, results, fields){
+          if(error)
+            res.json(error);
+          else
+            res.json(results);
+          // connection.end();
+          console.log('executou!');
+        });
+      }
+  }
+
+
+  connection.connect(function(err){
+    if(err) return console.log(err);
+    console.log('conectou');
+  });
