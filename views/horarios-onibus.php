ecc81c2eb77cbc8453cf5c48f46ae0e6e1e8c946
Bruna <brunasousadealmeida@gmail.com>
2018-05-14 07:36:02 -0300

    Alterando parte de compra

--- views/horarios-onibus.php
+++ views/horarios-onibus.php
@@ -23,6 +23,21 @@
     <link rel="stylesheet" href="<?php echo($links); ?>css/style_login.css">
     <link rel="stylesheet" href="<?php echo($links); ?>css/style_detalhes.css">
     <script src="<?php echo($links); ?>js/jquery.min.js"></script>
+    <script src="../js/jquery-3.3.1.js"></script>
+    <script>
+    function Selecionar(){
+    var selecionado = document.getElementById("poltronas");
+
+    // Se estiver amarelo, troca pra verde
+    if(selecionado.style.background === "green") {
+        selecionado.style.background = "yellow";
+
+    // Senão, troca pra amarelo
+    } else {
+        selecionado.style.background = "green";
+    }
+}
+</script>
     <script>
       // Modal Login
       $(document).ready(function() {
@@ -80,8 +95,6 @@
 
            <div class="lista_horarios">
              <form class="" action="Usuario/usuario_identificacao.php" method="post">
-
-
              <table class="tabela_horarios">
                <tr class="linha_horarios">
                  <td class="coluna_horarios">Compania</td>
@@ -111,7 +124,70 @@
                  <td class="coluna_horarios"><button class="" type="submit" name="btn_confirma">Selecionar</button></td>
                </tr>
              </table>
-             <div class=""><img src="../img/poltronas.png" alt="poltronas"></div>
+             <div class="container_onibus">
+               <div class="legenda">
+                 <div class="leg_box" style="background-color:green;"></div><div class="leg_text">Disponivel</div>
+                 <div class="leg_box" style="background-color:yellow;"></div><div class="leg_text">Selecionado</div>
+                 <div class="leg_box" style=""></div><div class="leg_text">Ocupado</div>
+               </div>
+               <div class="onibus">
+                 <div class="fileira">
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">1</div></a>
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">2</div></a>
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">3</div></a>
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">4</div></a>
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">5</div></a>
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">6</div></a>
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">7</div></a>
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">8</div></a>
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">9</div></a>
+                   <a href="#" onclick="Selecionar()"><div id="poltronas" style="background: green;">10</div></a>
+                 </div>
+                 <div class="fileira">
+                   <div id="poltronas">11</div>
+                   <div id="poltronas">12</div>
+                   <div id="poltronas">13</div>
+                   <div id="poltronas">14</div>
+                   <div id="poltronas">15</div>
+                   <div id="poltronas">16</div>
+                   <div id="poltronas">17</div>
+                   <div id="poltronas">18</div>
+                   <div id="poltronas">19</div>
+                   <div id="poltronas">20</div>
+                 </div>
+                 <br>
+                 <br>
+                 <div class="fileira">
+                   <div id="poltronas">21</div>
+                   <div id="poltronas">22</div>
+                   <div id="poltronas">23</div>
+                   <div id="poltronas">24</div>
+                   <div id="poltronas">25</div>
+                   <div id="poltronas">26</div>
+                   <div id="poltronas">27</div>
+                   <div id="poltronas">28</div>
+                   <div id="poltronas">29</div>
+                   <div id="poltronas">30</div>
+                 </div>
+                 <div class="fileira">
+                   <div id="poltronas">31</div>
+                   <div id="poltronas">32</div>
+                   <div id="poltronas">33</div>
+                   <div id="poltronas">34</div>
+                   <div id="poltronas">35</div>
+                   <div id="poltronas">36</div>
+                   <div id="poltronas">37</div>
+                   <div id="poltronas">38</div>
+                   <div id="poltronas">39</div>
+                   <div id="poltronas">40</div>
+                 </div>
+               </div>
+             </div>
+            <div class="Continua">
+              <a href="Usuario/usuario_identificacao.php">
+               Continuar
+              </a>
+            </div>
              </form>
            </div>
          </div>
